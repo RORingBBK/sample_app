a653407949f19d0cc30f2ad49ba261a9a34c9ca5
Bibek Khadka <bibek@vayu.com.au>
2016-12-26 00:57:47 +0545

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root  'static_pages#home'
   get   '/help',    to: 'static_pages#help'
   get   '/about',   to: 'static_pages#about'
@@ -15,5 +19,6 @@ Rails.application.routes.draw do
   resources :users
 
   resources :account_activation, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   
 end
